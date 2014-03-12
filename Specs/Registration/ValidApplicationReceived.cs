35fe62d365b7217dcf1899f1988f035fc534cbcd
Dave Mateer home <davemateer@gmail.com>
2014-03-12 07:33:22 +0000

    First commit - got first Scenario (ValidApplicationReceived) and 4 Specs (test) written

---
+++ Specs/Registration/ValidApplicationReceived.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Specs.Registration
+{
+    public class ValidApplicationReceived
+    {
+        [Fact]
+        public void User_Is_Added_To_System() { }
+        [Fact]
+        public void Log_Entry_Is_Created_For_Event() { }
+        [Fact]
+        public void Email_Sent_to_Confirm_Address() { }
+        [Fact]
+        public void A_Message_is_Provided_for_User() { }
+
+    }
+}
